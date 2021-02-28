f79a8d871f86d44c98743f7578545408fce20c8c
lastar38 <lastarmain@gmail.com>
2021-02-28 10:31:16 +0900

    新規作成

---
+++ Abbreviations/Abbreviations/Abbreviations.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abbreviations
+{
+    class Abbreviations
+    {
+        private Dictionary<string, string> _dict = new Dictionary<string, string>();
+
+        public Abbreviations()
+        {
+            var lines = File.ReadAllLines("Abbreviations.txt");
+            _dict = lines.Select(line => line.Split('='))
+                         .ToDictionary(x => x[0], x => x[1]);
+        }
+
+        public void Add(string abbr, string japanese)
+        {
+            _dict[abbr] = japanese;
+        }
+
+        public string this[string abbr]
+        {
+            get
+            {
+                return _dict.ContainsKey(abbr) ? _dict[abbr] : null;
+            }
+        }
+
+        public string ToAbbreviation(string japanese)
+        {
+            return _dict.FirstOrDefault(x => x.Value == japanese).Key;
+        }
+
+        public IEnumerable<KeyValuePair<string, string>> FindAll(string substring)
+        {
+            foreach (var item in _dict)
+            {
+                if (item.Value.Contains(substring))
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+}
---
+++ Abbreviations/Abbreviations/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abbreviations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var abbrs = new Abbreviations();
+
+            abbrs.Add("IOC", "国際オリンピック委員会");
+            abbrs.Add("NPT", "核兵器不拡散条約");
+
+            var names = new[] { "WHO", "FIFA", "NPT", };
+            foreach (var name in names)
+            {
+                var fullname = abbrs[name];
+                if(fullname == null)
+                {
+                    Console.WriteLine("{0}は見つかりません", name);
+                }
+                else
+                {
+                    Console.WriteLine("{0}={1}", name, fullname);
+                }
+            }
+            Console.WriteLine();
+
+            var japanese = "東南アジア諸国連合";
+            var abbreviation = abbrs.ToAbbreviation(japanese);
+            if (abbreviation == null)
+            {
+                Console.WriteLine("{0}は見つかりません", japanese);
+            }
+            else
+            {
+                Console.WriteLine("「{0}」の略語は{1}です", japanese, abbreviation);
+            }
+            Console.WriteLine();
+
+            foreach (var item in abbrs.FindAll("国際"))
+            {
+                Console.WriteLine("{0}={1}", item.Key, item.Value);
+            }
+        }
+    }
+}
